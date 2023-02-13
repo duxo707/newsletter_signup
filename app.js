d26d37eb7e52b2378f451748d79d525f4d9cefd7
duxo707 <96288436+duxo707@users.noreply.github.com>
2023-02-13 19:44:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 mailChimp.setConfig({
-    apiKey: "366388a7f5d566f73b0b732d051a74dc-us21",
+    apiKey: "",
     server: "us21"
 });
 
@@ -15,7 +15,7 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 app.post("/", function (req, res) {
-    const listId = "fce1a62d3f";
+    const listId = "";
     const newUser = {
         firstName: req.body.fname,
         lastName: req.body.lname,
@@ -41,6 +41,3 @@ app.post("/failure", function (req, res) {
 app.listen(3000, function () {
     console.log("sever is running on port 3000");
 });
-
-//api key : 366388a7f5d566f73b0b732d051a74dc-us21
-//audience or list id: fce1a62d3f
